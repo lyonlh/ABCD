f659de96427bf125c7f874f084b618be5217c91e
lihang <lyonlh@gmail.com>
2018-02-26 08:14:13 +0800

    Avoid additional word split such as -DEMACS_NAME="GNU Emacs".

--- cc-wrapper.sh
+++ cc-wrapper.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 path_name=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
-compiler_wrapper="$path_name/compiler-wrapper.sh"
+compiler_wrapper="$path_name/compiler-wrapper"
 config="$path_name/config"
+
+source $compiler_wrapper
 source $config
 
-$compiler_wrapper "$CC" "$@"
+generate_compile_command $CC "$@"
+echo -n -e "$item" >> $db_file
+
+$CC "$@"
--- make-wrapper.sh
+++ make-wrapper.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 path_name=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
-# path_name=$_
-
-source "$path_name/utils"
-
 cc_wrapper="$path_name/cc-wrapper.sh"
 cxx_wrapper="$path_name/cxx-wrapper.sh"
 config="$path_name/config"
+utils="$path_name/utils"
+source $utils
 
 # Retrive CC/CXX from make database
 ORIGIN_CC="cc"
@@ -22,21 +20,21 @@ while read compiler assign value; do \
         fi \
     fi \
     done <<EOF
-$(make -spqRr "$@" 2>/dev/null)
+$(make -pqRr "$@" 2>/dev/null)
 EOF
 
 # Generate file from which compiler-wrapper.sh can read config
-echo "db_file=$(pwd)/compile_commands.json" >$config
+db_file="$(pwd)/compile_commands.json"
+echo "db_file=$(quote $db_file)" >$config
 echo "CC=$(quote $ORIGIN_CC)" >>$config
 echo "CXX=$(quote $ORIGIN_CXX)" >>$config
-source $config
 
 echo ====================
 cat $config
 echo ====================
 
 echo "[" > $db_file
-make -j1 -Bsk "$@" CC="$(one_word $cc_wrapper)" CXX="$(one_word $cxx_wrapper)"
+make -j1 -k "$@" CC="$cc_wrapper" CXX="$cxx_wrapper"
 echo "]" >> $db_file
 
 rm -f $config &>/dev/null
