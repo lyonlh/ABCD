69426f29d8c705b478188a39dbd2ae7c23dbf493
lihang <lyonlh@gmail.com>
2018-03-06 23:11:51 +0800

    Fix some warning from ShellCheck.

--- mint.sh
+++ mint.sh
@@ -4,19 +4,18 @@ my_path=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 cc_wrapper="$my_path/cc-wrapper.sh"
 cxx_wrapper="$my_path/cxx-wrapper.sh"
 cpp_wrapper="$my_path/cpp-wrapper.sh"
-utils="$my_path/utils"
-source $utils
+source "$my_path/utils"
 db_file="$(pwd)/compile_commands.json"
 
-usage="usage: mint.sh [make options] [-- [-a] [-d] [-h] [-o db_file]]\n \
--a\t\tuse 'arguments' instead of 'command' field in compilation database \n \
--d\t\tprint debug message\n \
--h\t\tdisplay this help and exit\n \
--o db_file\twrite output to the db_file\n \
+usage="usage: mint.sh [make options] [-- [-a] [-d] [-h] [-o db_file]]\\n \
+-a\\t\\tuse 'arguments' instead of 'command' field in compilation database \\n \
+-d\\t\\tprint debug message\\n \
+-h\\t\\tdisplay this help and exit\\n \
+-o db_file\\twrite output to the db_file\\n \
 "
 
 # Extract options with respective to make and this wrapper
-if [[ $@ =~ (.*)--(.*) ]]
+if [[ $* =~ (.*)--(.*) ]]
 then
     make_opts=${BASH_REMATCH[1]}
     if [[ ${#BASH_REMATCH[@]} == 3 ]]
@@ -24,7 +23,7 @@ then
         wrapper_opts=${BASH_REMATCH[2]}
     fi
 else
-    make_opts=$@
+    make_opts=$*
 fi
 
 # Parse options for this wrapper
@@ -36,7 +35,7 @@ then
             a) use_arg_field=1;;
             d) debug=1;;
             o) db_file="$(cd $(dirname $OPTARG) && pwd)/$(basename $OPTARG)";;
-            h|*) printf %b "$usage\n"; exit 0;;
+            h|*) printf %b "$usage\\n"; exit 0;;
         esac
     done
 fi
@@ -46,7 +45,7 @@ fi
 ORIGIN_CC="cc"
 ORIGIN_CXX="c++"
 ORIGIN_CPP=""
-while read compiler assign value
+while read -r compiler assign value
 do
     if [[ $assign =~ :?= && "$value" ]]
     then
@@ -67,19 +66,19 @@ EOF
 
 # Generate file from which CC/CXX-wrapper can read global enviroment
 global_env="$my_path/global-env"
-printf "%s\n%s\n%s\n%s\n%s\n" \
+printf "%s\\n%s\\n%s\\n%s\\n%s\\n" \
        "db_file=$(quote $db_file)" \
        "CC=$(quote $ORIGIN_CC)" \
        "CXX=$(quote $ORIGIN_CXX)" \
        "CPP=$(quote $ORIGIN_CPP)" \
        "debug=$(quote $debug)" \
-       "use_arg_field=$(quote $use_arg_field)" > $global_env && \
-    trap "rm -f $global_env" INT TERM EXIT
+       "use_arg_field=$(quote $use_arg_field)" > "$global_env" && \
+    trap 'rm -f "$global_env"' INT TERM EXIT
 
-debug_log $(cat $global_env)
+debug_log $(< "$global_env")
 
-printf "[\n" > $db_file
+printf "[\\n" > "$db_file"
 make $make_opts CC="$cc_wrapper" CXX="$cxx_wrapper" ${ORIGIN_CPP:+CPP="$cpp_wrapper"}
 # Delete tail comma
-sed -i -n -e '$!p' $db_file
-printf " }\n]\n" >> $db_file
+sed -i -n -e '$!p' "$db_file"
+printf " }\\n]\\n" >> "$db_file"
